1cb02f96b60144afb4933b7e886a87c56ed4a380
Denys Vasyliev <den@msrn.me>
2017-11-24 01:55:00 +0200

    fix

--- jobs/test.groovy
+++ jobs/test.groovy
@@ -65,14 +65,14 @@ manager.listener.logger.println("commit notification hd387ry34oiuhr3ofin")
 manager.listener.logger.println("* [new tag] v0.13.6 -> v.0.13.6")
 
 
-pattern_commit = ~/commit notification\s*(.*)/
-pattern_version = ~/\[new tag\]\s*(.*)/
+pattern_commit = ~/commit notification.*(.*)/
+pattern_version = ~/[new tag].s*(.*)/
 
 manager.build.logFile.eachLine { 
    line -> l=line
  
- try {commit=(l =~ /commit notification\s*(.*)/)[0][1]} catch(Exception ex) {;}
- try {version=(l =~ /.*\[new tag\]\s*(.*)/)[0][1]} catch(Exception ex) {;}
+ try {commit=(l =~ /commit notification.*(.*)/)[0][1]} catch(Exception ex) {;}
+ try {version=(l =~ /.*[new tag].*(.*)/)[0][1]} catch(Exception ex) {;}
 
 }
 
@@ -81,7 +81,7 @@ manager.addShortText("<a href=https://github.com/den-vasyliev/nodejs/commit/$com
 manager.listener.logger.println("I want to see this line in my job's output: $commit")
 manager.addBadge("star-gold.gif", "icon from greenballs plugin")  
         '''.stripIndent().trim()
-        
+
         junit {
             testResults('**/junit.xml')
             allowEmptyResults(true)
