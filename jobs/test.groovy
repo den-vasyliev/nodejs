8e6791272f25f6073cff9a9564870865cb991e23
Denys Vasyliev <den@msrn.me>
2017-11-24 01:52:20 +0200

    fix

--- jobs/test.groovy
+++ jobs/test.groovy
@@ -61,9 +61,27 @@ job('publish_qlmm') {
 
     publishers {
         groovyPostBuild '''
-            manager.listener.logger.println 'Do some groovy post processing!'
-            
+manager.listener.logger.println("commit notification hd387ry34oiuhr3ofin")
+manager.listener.logger.println("* [new tag] v0.13.6 -> v.0.13.6")
+
+
+pattern_commit = ~/commit notification\s*(.*)/
+pattern_version = ~/\[new tag\]\s*(.*)/
+
+manager.build.logFile.eachLine { 
+   line -> l=line
+ 
+ try {commit=(l =~ /commit notification\s*(.*)/)[0][1]} catch(Exception ex) {;}
+ try {version=(l =~ /.*\[new tag\]\s*(.*)/)[0][1]} catch(Exception ex) {;}
+
+}
+
+
+manager.addShortText("<a href=https://github.com/den-vasyliev/nodejs/commit/$commit target=_blank>$version</a>")
+manager.listener.logger.println("I want to see this line in my job's output: $commit")
+manager.addBadge("star-gold.gif", "icon from greenballs plugin")  
         '''.stripIndent().trim()
+        
         junit {
             testResults('**/junit.xml')
             allowEmptyResults(true)
